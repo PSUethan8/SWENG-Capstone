01a313816211709b1ebca751f00c9fae410838fc
Kenton Porter <kjpy2k@gmail.com>
2026-09-12 16:18:16 -0400

    Initial Commit

---
+++ GridLine/GridLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridLineApp.swift
+//  GridLine
+//
+//  Created by Kenton Porter on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct GridLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GridLineTests/GridLineTests.swift
@@ -0,0 +1,17 @@
+//
+//  GridLineTests.swift
+//  GridLineTests
+//
+//  Created by Kenton Porter on 9/12/26.
+//
+
+import Testing
+@testable import GridLine
+
+struct GridLineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
